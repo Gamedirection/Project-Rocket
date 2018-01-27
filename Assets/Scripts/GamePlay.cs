f9e3f5232b136c69894436437f371f8a6442f309
Michael <michael@outlierinteractive.com>
2018-01-27 02:26:08 -0500

    Gameplay added. Text only.

--- Assets/Scripts/GamePlay.cs
+++ Assets/Scripts/GamePlay.cs
@@ -2,15 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GamePlay : MonoBehaviour {
+[System.Serializable]
+public class GamePlay {
 
-	// Use this for initialization
-	void Start () {
-		
+	public Player player1;
+	public Player player2;
+	public bool gameOver = false;
+
+	public void ExecuteTurn() {
+		//Undefend player each turn
+		player1.Defending = false;
+		player2.Defending = false;
+
+		//Get each player's action from their queues
+		PlayerAction player1Action = (player1.actionQueue.Count > 0) ? player1.actionQueue.Dequeue() : PlayerAction.Nothing();
+		PlayerAction player2Action = (player2.actionQueue.Count > 0) ? player2.actionQueue.Dequeue() : PlayerAction.Nothing();
+
+		//Execute the action with the highest priority, then the other action
+		if(player1Action.actionType <= player2Action.actionType) {
+			player1.ExecuteAction(player1Action, player2);
+			player2.ExecuteAction(player2Action, player1);
+		}
+		else {
+			player2.ExecuteAction(player2Action, player1);
+			player1.ExecuteAction(player1Action, player2);
+		}
+
+		//Did anyone die?
+		EvaluateEndGame();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void EvaluateEndGame() {
+		if(player1.Health <= 0 || player2.Health <= 0) {
+			gameOver = true;
+			Debug.Log("\n=========Game Over!==============");
+		}
 	}
+
+	public bool AllMovesCompleted() {
+		if(player1.actionQueue.Count == 0 && player2.actionQueue.Count == 0) {
+			Debug.Log("\nAll Moves Finished.");
+			return true;
+		}
+		return false;
+	}
+
 }
