05d87e3a7c5721623154b91dc94959b485ff5a16
Michael <michael@outlierinteractive.com>
2018-01-27 05:04:48 -0500

    Working on Main Menu UI

---
+++ Assets/Scripts/MainMenuUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuUI : MonoBehaviour {
+
+	public enum MainMenuScreen { MainMenu, Instructions, Options, Credits }
+	public MainMenuScreen curScreen = MainMenuScreen.MainMenu;
+
+	public RectTransform MainMenuBar;
+	public int mainMenuItem = 0;
+
+	public RectTransform OptionsBar;
+
+	void Update() {
+		if(Input.GetKeyDown(KeyCode.LeftArrow))
+			mainMenuItem = (mainMenuItem + 5 - 1) % 5;
+		if(Input.GetKeyDown(KeyCode.RightArrow))
+			mainMenuItem = (mainMenuItem + 1) % 5;
+
+		//-150, -475
+		float startPoint = MainMenuBar.sizeDelta.x / 5f / 2f;
+		float offset = MainMenuBar.sizeDelta.x / 5f;
+		Vector2 goalPosition = new Vector2( -startPoint - offset * (mainMenuItem), 0);
+		MainMenuBar.anchoredPosition = Vector2.Lerp( MainMenuBar.anchoredPosition, goalPosition, Time.deltaTime * 3f);
+
+			
+	}
+
+
+
+}
