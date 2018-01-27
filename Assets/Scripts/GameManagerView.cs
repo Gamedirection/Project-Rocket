4feb48c1c54d0aad21ef12eacdb524333bb29c7f
Michael <michael@outlierinteractive.com>
2018-01-27 08:01:32 -0500

    Added 3D representation of abstract game state.

---
+++ Assets/Scripts/GameManagerView.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManagerView : MonoBehaviour {
+
+	public GameManager manager;
+	public Transform player1Model;
+	public Transform player2Model;
+
+	public Vector3[] player1Positions;
+	public Vector3[] player2Positions;
+
+	public Text player1MovesText, player2MovesText;
+
+	void Update () {
+		player1Model.position = Vector3.Lerp(player1Model.position, player1Positions[manager.player1.Position], Time.deltaTime * 5f);
+		player2Model.position = Vector3.Lerp(player2Model.position, player2Positions[manager.player2.Position], Time.deltaTime * 5f);
+
+		player1MovesText.text = "Moves:" + new string('▣', manager.player1.actionQueue.Count);
+		player2MovesText.text = new string('▣', manager.player2.actionQueue.Count) + ":Moves";
+	}
+}
